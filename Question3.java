0015fae57616cda2e49fa02d7178d92f93899f5a
PremaGowtham <premalathamegala@gmail.com>
2024-08-18 21:09:53 +0530

    added Question3

---
+++ Question3.java
@@ -0,0 +1,13 @@
+package Task4;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		
+		for(char ch = 'A'; ch <= 'Z'; ch++)
+		{
+			System.out.println(ch + " ");
+		}
+	}
+
+}
