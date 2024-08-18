2df1e26a2a0b1e52a1107f99701ddce53541ea25
PremaGowtham <premalathamegala@gmail.com>
2024-08-18 21:12:12 +0530

    added Question7

---
+++ Question7.java
@@ -0,0 +1,21 @@
+package Task4;
+
+import java.util.Scanner;
+
+public class Question7 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter the string");
+		String inputString = scan.nextLine();
+		
+		int length = inputString.length();
+		
+		System.out.println("The length of the string is:" + length);
+		
+
+	}
+
+}
