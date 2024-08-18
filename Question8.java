30bbf7845770358bc884376e864dd42955368ac2
PremaGowtham <premalathamegala@gmail.com>
2024-08-18 21:12:30 +0530

    added Question8

---
+++ Question8.java
@@ -0,0 +1,14 @@
+package Task4;
+
+public class Question8 {
+
+	public static void main(String[] args) {
+		
+		
+		for(int i=1; i <=10; i++) {
+			
+		System.out.println("Welcome to Guvi");
+		}
+	}
+
+}
